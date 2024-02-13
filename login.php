140aa05bb18418c765b0460fee7f8418fd1b6269
mileto888 <159888150+mileto888@users.noreply.github.com>
2024-02-13 11:55:50 +0200

    novo

--- login.php
+++ login.php
@@ -1,3 +1,45 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "1234";
+	$database = "project";
+
+	try {
+	  $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+	  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  
+	} catch(PDOException $e) {
+	  echo "Connection failed: " . $e->getMessage();
+	}
+  if ( isset( $_POST['submit'] ) ) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$sql = "INSERT INTO users ( name, email, password) VALUES (?,?,?)";
+		$connection->prepare($sql)->execute([$name, $email, $password]);
+	}
+
+
+
+  if ( isset( $_POST['submit2'] ) ) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $stmt = $connection->prepare("SELECT * FROM users WHERE email = ? AND password = ?"); 
+    $stmt->execute([ $email, $password ]); 
+    $user = $stmt->fetch();
+    if ( $user ) {
+      $_SESSION['user'] = $user;
+      header("location: index.php");
+      exit;
+      
+    } else {
+      
+      echo "<b style='color:red;'>Невалидни потребителски данни</b><br><br>";
+    }
+  }
+     ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,7 +49,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="login.css">
   
   </head>
@@ -25,19 +67,19 @@
     </span>
     <div class="form-box login">
       <h2>Login</h2>
-      <form action="#">
+      <form action="#" method="post">
 
         <div class="input-box">
           <span class="icon">
              <ion-icon name="mail"></ion-icon>
           </span>
-             <input type="email"  required>
+             <input type="email" name="email" required>
             <label>Email</label>
         </div>
 
         <div class="input-box">
           <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-          <input type="password"required>
+          <input type="password" name="password" required>
           <label>Password</label>
         </div>
         <div class="remember-forgot">
@@ -45,7 +87,7 @@
           Remember me</label>
           <a href="#">Forgot Password</a>
         </div>
-        <button type="submit" class="btn">Login</button>
+        <button type="submit" name="submit2" value="2" class="btn">Login</button>
         <div class="login-register">
           <p>Don't have an account? 
             <a href="#" class="register-link">Register</a>
@@ -58,7 +100,7 @@
 
     <div class="form-box register">
       <h2>Registration</h2>
-      <form action="#">
+      <form action="#"  method="post">
 
         <div class="input-box">
           <span class="icon">
@@ -88,7 +130,7 @@
          
         </div>
 
-        <button type="submit" name="submit" class="btn">Register</button>
+        <button type="submit" name="submit" value="1" class="btn">Register</button>
         <div class="login-register">
           <p>Already have an account? 
             <a href="#" class="login-link">Login</a>
